81a606fbcfd3082da431555cdbd3339b7b0cda1f
Vladimir Komarov <fogfear@gmail.com>
2016-12-24 22:58:06 +0200

    Add several function

--- sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,10 +2,30 @@ package ua.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-    public static void main(String[] agrs) {
+  public static void main(String[] agrs) {
 
-      System.out.println("Hello, World!");
+    hello("world");
+    hello("user");
+    hello("Volodymyr");
 
-    }
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + " + " + area(l));
 
-}	
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольник со сторонами " + a + " и " + b + " = " + area(a, b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
+}
+
