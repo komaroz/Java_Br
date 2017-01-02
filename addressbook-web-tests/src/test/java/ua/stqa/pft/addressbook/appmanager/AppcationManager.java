763d2a6c69eff0d5b8c23e3b1d15eb7189660d85
Vladimir Komarov <fogfear@gmail.com>
2017-01-02 18:17:24 +0200

    Change applmanager

--- addressbook-web-tests/src/test/java/ua/stqa/pft/addressbook/appmanager/AppcationManager.java
+++ addressbook-web-tests/src/test/java/ua/stqa/pft/addressbook/appmanager/AppcationManager.java
@@ -1,8 +1,6 @@
 package ua.stqa.pft.addressbook.appmanager;
 
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -13,15 +11,6 @@ public class AppcationManager {
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupsHelper groupsHelper;
-
-  public static boolean isAlertPresent(FirefoxDriver wd) {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
 
   public void init() {
     System.setProperty("webdriver.gecko.driver", "C:\\geckodriver-v0.11.1-win64\\geckodriver.exe");
--- addressbook-web-tests/src/test/java/ua/stqa/pft/addressbook/appmanager/GroupsHelper.java
+++ addressbook-web-tests/src/test/java/ua/stqa/pft/addressbook/appmanager/GroupsHelper.java
@@ -1,48 +1,40 @@
 package ua.stqa.pft.addressbook.appmanager;
 
+import com.sun.javafx.binding.ExpressionHelperBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ua.stqa.pft.addressbook.model.GroupData;
 
-/**
- * Created by user on 02.01.2017.
- */
-public class GroupsHelper {
-  private FirefoxDriver wd;
+
+public class GroupsHelper extends HelperBase {
 
   public GroupsHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void returnToGroupPage() {
-    wd.findElement(By.linkText("group page")).click();
+    click(By.linkText("group page"));
   }
 
   public void submitGroupCreation() {
-    wd.findElement(By.name("submit")).click();
+    click(By.name("submit"));
   }
 
   public void fillGroupForm(GroupData groupData) {
-    wd.findElement(By.name("group_name")).click();
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    wd.findElement(By.name("group_header")).click();
-    wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    wd.findElement(By.name("group_footer")).click();
-    wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    type(By.name("group_name"), groupData.getName());
+    type(By.name("group_header"), groupData.getHeader());
+    type(By.name("group_footer"), groupData.getFooter());
   }
 
   public void initGroupCreation() {
-    wd.findElement(By.name("new")).click();
+    click(By.name("new"));
   }
 
   public void deleteSelectedGroups() {
-    wd.findElement(By.name("delete")).click();
+    click(By.name("delete"));
   }
 
   public void selectGroup() {
-    wd.findElement(By.name("selected[]")).click();
+    click(By.name("selected[]"));
   }
 }
